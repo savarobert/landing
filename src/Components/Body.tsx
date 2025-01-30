8d95b6659cf65bed669854d2ca2488004b35ffc5
savar <sava.robert>
2025-01-30 10:47:36 +0100

    Update deploy script to not remove cname record

--- src/Components/Body.tsx
+++ src/Components/Body.tsx
@@ -1,8 +1,14 @@
+import { Typography } from "@mui/material"
+
 function Body() {
   
     return (
       <>
-        <h1>Body</h1>
+                <Typography padding={2} >                    
+                    <Typography variant="body1" paddingBottom={2}>Hi, I'm Rob!</Typography>
+                    <Typography variant="body1" paddingBottom={2}>Software engineer by day (and sometimes night), I love to build new things.</Typography>
+                    <Typography variant="body1" paddingBottom={2}>Also something of a music nerd (guitar and bass), tattoos, gaming enthusiast, and a fan of all things tech.</Typography>                    
+                </Typography>
       </>
     )
   }
